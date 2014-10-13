5cb396ba84741f9eb86b2ec3362348c77a710fc9
Griffin Crane <crane.griffin@gmail.com>
2014-10-13 10:11:06 -0500

    update 1 -- can change tiles by clicking or touch
    works on Android phones and Windows PC, PC tools to be built-in to Unity
    editor, in-game GUI to compensate on mobile devices to come

--- Assets/script/TileMap.cs
+++ Assets/script/TileMap.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class TileMap : MonoBehaviour {
 
+	private bool oneTouch = false;
+
     /** the tile atlas, a texture containing one or more tile images evenly spaced **/
     public Texture2D atlas;
+
+	private int[,] map;
+	private GameObject[,] tileSprites;
 
     // TODO: account for tile atlas padding
 
@@ -35,25 +40,49 @@ public class TileMap : MonoBehaviour {
 		return new Rect ((tilecode % AtlasWidth) * tileWidth, ((AtlasHeight - (tilecode / AtlasWidth)) * tileHeight) - tileHeight, tileWidth, tileHeight);
 	}
 
-    public GameObject DrawTile(int x, int y, int i, int j, int tilecode)
+    public GameObject DrawTile(int x, int y, int tilecode)
     {
+		if (tilecode == -1) 
+		{
+			// disable this tile
+			tileSprites[y, x].renderer.enabled = false;
+			return tileSprites[y, x];
+		} else if (tileSprites[y, x] != null) {
+			tileSprites[y, x].renderer.enabled = true;
+		}
+
 		if (tilecode >= (AtlasWidth * AtlasHeight)) {
 			throw new System.IndexOutOfRangeException("Tile code index is out of range of tile atlas.");
 		}
 	
-    	//extract tile from atlas into sprite
-		GameObject go = new GameObject("Tile (" + ((2 * x) + i) + ", " + ((2 * y) + j) + "): " + tilecode, typeof(SpriteRenderer));
-		((SpriteRenderer)(go.renderer)).sprite = 
-			Sprite.Create(atlas, getTileRect(tilecode), Vector2.zero);
+		// SINGLETON
+		// if a sprite exists for this tile already, replace it with a new tile sprite
+		if (tileSprites[y, x] != null) {
+			((SpriteRenderer)tileSprites[y, x].renderer).sprite = Sprite.Create(atlas, getTileRect(tilecode), Vector2.zero);
+			return tileSprites[y, x];
+		}
 
-		go.transform.Translate ((i / 100f) * 32, (j / 100f) * 32, 0f);
+    	// create a new sprite if one does not exist already
+		if (tileSprites[y, x] == null) {
+			GameObject go = new GameObject("Tile: " + tilecode, typeof(SpriteRenderer));
+			((SpriteRenderer)(go.renderer)).sprite = 
+				Sprite.Create(atlas, getTileRect(tilecode), Vector2.zero);
+			go.transform.Translate ((x / 100f) * 32, (y / 100f) * 32, 0f);
 
-		return go;
+			tileSprites[y, x] = go;
+			return tileSprites[y, x];
+		}
+
+		// TODO: shouldn't get here yet
+		return null;
     }
 
 	// Use this for initialization
 	void Start () {
-		int[,] map = new int[AtlasHeight, AtlasWidth];
+		map = new int[AtlasHeight, AtlasWidth];
+		// TODO: on a mostly empty map, don't use so much memory space
+		// although these ARE pointers per C#
+		tileSprites = new GameObject[AtlasHeight, AtlasWidth];
 
 		// print all atlas tiles in order
 		int tc = 0;
@@ -68,30 +97,82 @@ public class TileMap : MonoBehaviour {
         // turn off texture filtering
         atlas.filterMode = FilterMode.Point;
 
-		// quad count
-		int c = 0;
+		// render all tiles in map
+		DrawRegion (0, 0, AtlasWidth, AtlasHeight);
+	}
 
-	    for (int y = 0; y < AtlasWidth; y += 2) {
-	        for (int x = 0; x < AtlasHeight; x += 2) {
-				GameObject quad = new GameObject("Quad " + c);
-				quad.transform.Translate((x / 100f) * 32, (y / 100f) * 32, 0f);
-				quad.transform.parent = this.transform;
+	void DrawRegion(int x, int y, int width, int height) 
+	{
+		for (int j = y; j < y + height; j++) 
+		{
+			for (int i = x; i < x + width; i++) 
+			{
+				tileSprites[j, i] = DrawTile(i, j, map[j, i]);
+			}
+		}
+	}
 
-				for (int j = y; j < y + 2; j++) {
-					for (int i = x; i < x + 2; i++) {
-						if (i > -1 && i < map.GetLength(0)
-						    && j > -1 && j < map.GetLength(1)) {							
-							int tilecode = map[j, i];
-							//Debug.Log("Source rect of " + tilecode + " = " + getTileRect(tilecode));
-							GameObject newTile = DrawTile(x, y, i, j, tilecode);
-							newTile.transform.parent = quad.transform;
-						}
+	void AddTile (int x, int y, int tilecode) 
+	{
+		if (tilecode < (AtlasWidth * AtlasHeight)) {
+			map [y, x] = tilecode;
+		} else {
+			map [y, x] = -1;
+		}
+
+		DrawTile (x, y, map [y, x]);
+	}
+
+	void RemoveTile (int x, int y)
+	{
+		// update map data
+		map [y, x] = -1;
+
+		// update tile sprite
+		DrawTile (x, y, -1);
+	}
+
+	void Update() {
+		if (Input.touchCount == 0) {
+			oneTouch = false;
+		}
+
+		if (Input.GetMouseButtonDown (0) || Input.touchCount == 1) {
+
+			Ray ray = new Ray();
+
+			if (Input.GetMouseButtonDown(0)) {
+				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			} else if (Input.touchCount == 1 && !oneTouch) { 
+				ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+				oneTouch = true;
+			} else return;
+			
+			Debug.Log ("Got a click at: " + ray);
+			
+			if (Physics2D.Raycast(ray.origin, ray.direction)) {
+				int tileX = (int)(ray.origin.x * 100f) / 32;
+				int tileY = (int)(ray.origin.y * 100f) / 32;
+				int tilecode = (tileY * 32) + tileX;
+				
+				Debug.Log ("The mouse hit a tile: (" +
+				           tileX + ", " + tileY + "): " + tilecode);
+
+				// check tilemap boundaries
+				if (tileX < map.GetLength(1) && tileY < map.GetLength(0)) {
+					// loop through all tilecodes
+					if (map[tileY, tileX] == (AtlasWidth * AtlasHeight) - 1) {
+						// loop to first tile code at last tilecode
+						map[tileY, tileX] = 0;
+					} else {
+						// otherwise iterate ahead by one
+						map[tileY, tileX] = map[tileY, tileX] + 1;
 					}
-				}
 
-				// increment quad count
-				c++;
-	        }
-	    }
+					// create / render new sprite image
+					DrawTile(tileX, tileY, map[tileY, tileX]);
+				}
+			}
+		}
 	}
 }
