a147456d8d0f65003dd3e2d15cb050a9510d8898
Griffin Crane <crane.griffin@gmail.com>
2014-11-07 16:01:10 -0600

    Revert "refactored custom inspector for TileMap"
    This reverts commit 29897288a17372f31a8fa37e3d837dd78eefd969.

--- Assets/Editor/TileMapPaletteInspector.cs
+++ Assets/Editor/TileMapPaletteInspector.cs
@@ -4,6 +4,138 @@ using System.Collections;
 
 [CustomEditor (typeof(TileMap))]
 public class TileMapPaletteInspector : Editor {
+	int selected;
+	int lastSelected;
+	int tileWidth;
+	int tileHeight;
+
+	Color[] lastTile;
+	Vector2 scrollPos = new Vector2 ();
+
+	private Texture2D[] texList;
+	private static GUIStyle blankStyle;
+	private static GUIStyle warningStyle;
+	private static GUIStyle buttonStyle;
+	
+	public override void OnInspectorGUI() {
+		// get tilemap as target object
+		var targetTilemap = target as TileMap;
+
+		// before any user input, clean the GUI dirty flag
+		GUI.changed = false;
+
+		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
+		targetTilemap.atlas = (Texture2D)EditorGUILayout.ObjectField ("Atlas Texture", targetTilemap.atlas, typeof(Texture2D), false);
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label ("Tile Size (x):");
+			targetTilemap.tileWidth = EditorGUILayout.IntField (targetTilemap.tileWidth);
+		GUILayout.EndHorizontal ();
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label ("Tile Size (y):");
+			targetTilemap.tileHeight = EditorGUILayout.IntField (targetTilemap.tileHeight);
+		GUILayout.EndHorizontal ();	
+		
+		// only define blankStyle once
+		if (blankStyle == null) {
+			blankStyle = new GUIStyle();
+		}
+		
+		// only define warningStyle once
+		if (warningStyle == null) {
+			warningStyle = new GUIStyle();
+			warningStyle.fontStyle = FontStyle.Bold;
+		}
+		
+		// only define buttonStyle once
+		if (buttonStyle == null) {
+			buttonStyle = new GUIStyle();
+			buttonStyle.stretchHeight = false;
+			buttonStyle.stretchWidth = false;
+			buttonStyle.padding = new RectOffset(2, 2, 2, 2);
+		}
+		
+		if (targetTilemap.atlas != null) {
+			// all tiles must be at least 2x2
+			if (targetTilemap.tileWidth > 1 && targetTilemap.tileHeight > 1) {
+				int numTilesX = targetTilemap.atlas.width / targetTilemap.tileWidth;
+				int numTilesY = targetTilemap.atlas.height / targetTilemap.tileHeight;
+				int numTiles = numTilesX * numTilesY;
+				
+				GUILayout.BeginHorizontal ();
+				if (numTiles > 4096) {
+					GUILayout.Label ("Divide into " + numTiles + " tiles?", warningStyle);
+				}
+				
+				if (numTiles <= 4096) {
+					GUILayout.Label ("Divide into " + numTiles + " tiles?");
+				}
+				
+				if (GUILayout.Button ("Apply") || texList == null) {
+					// cleanup unused textures
+					if (texList != null) {
+						foreach (Texture2D tex in texList) 
+						{
+							DestroyImmediate(tex);
+						}
+					}
+					
+					// destroy focused background texture if it exists
+					if (buttonStyle.onNormal.background != null) {
+						DestroyImmediate(buttonStyle.onNormal.background); 
+					}
+
+					// reset focused background size
+					buttonStyle.onNormal.background = 
+						new Texture2D(targetTilemap.tileWidth, targetTilemap.tileHeight);
+					Color[] backColors = new Color[targetTilemap.tileWidth * targetTilemap.tileHeight];
+					for (int i = 0; i < backColors.Length; i++) {
+						backColors[i] = Color.black;
+					}
+					
+					buttonStyle.onNormal.background.SetPixels(backColors);
+					buttonStyle.onNormal.background.hideFlags = HideFlags.DontSave;
+					buttonStyle.onNormal.background.Apply();
+
+					// create a list that contains all tiles in the atlas 
+					// divided into target tile sizes
+					texList = new Texture2D[(targetTilemap.atlas.width / targetTilemap.tileWidth) * (targetTilemap.atlas.height / targetTilemap.tileHeight)];
+					int k = 0;
+					
+					// flip array orientation by reading y-axis from top to bottom
+					for (int j = targetTilemap.atlas.height - targetTilemap.tileHeight; j >= 0; j -= targetTilemap.tileHeight) {
+						// continue reading x-axis left to right
+						for (int i = 0; i < targetTilemap.atlas.width; i += targetTilemap.tileWidth) {
+							texList [k] = new Texture2D (targetTilemap.tileWidth, targetTilemap.tileHeight);
+							texList [k].filterMode = FilterMode.Point;
+							texList [k].SetPixels (targetTilemap.atlas.GetPixels (i, j, targetTilemap.tileWidth, targetTilemap.tileHeight));
+							texList [k].hideFlags = HideFlags.DontSave;
+							texList [k].Apply ();
+							k++;
+						}
+					}
+				}
+				GUILayout.EndHorizontal ();
+			}			
+
+			if (texList != null) {
+			    int rowLength = (targetTilemap.atlas.width / targetTilemap.tileWidth);
+
+				if (rowLength > 0) {
+					scrollPos = GUILayout.BeginScrollView (scrollPos, blankStyle);
+					selected = GUILayout.SelectionGrid (selected, texList, rowLength, buttonStyle);
+					GUILayout.EndScrollView ();
+				}
+			}
+		}
+
+		// set tilemap data as dirty if the GUI is changed
+		// Unity will then save any changes and clear the dirty flag
+		if (GUI.changed)
+			EditorUtility.SetDirty (targetTilemap);
+	}
+
 	void OnSceneGUI () {
 		// get tilemap as target object
 		var targetTilemap = target as TileMap;
@@ -28,12 +160,11 @@ public class TileMapPaletteInspector : Editor {
                 	-(worldPoint.y - (2 * cameraPosition.y)));
 
 				// get x and y in tiles
-				Atlas atlas = targetTilemap.GetComponent<Atlas>();
-				int tileX = (int)((shiftPoint.x * 100) / atlas.tileWidth);
-				int tileY = (int)((shiftPoint.y * 100) / atlas.tileHeight);
+				int tileX = (int)((shiftPoint.x * 100) / targetTilemap.tileWidth);
+				int tileY = (int)((shiftPoint.y * 100) / targetTilemap.tileHeight);
 
 				// use TileMap method to draw tile to TileMap
-				if (targetTilemap.DrawTile(tileX, tileY, atlas.selected) == null) {
+				if (targetTilemap.DrawTile(tileX, tileY, selected) == null) {
 					// switch control back to the Editor if clicking outside of TileMap
 					GUIUtility.hotControl = 0;
 				}
--- Assets/script/TileMap.cs
+++ Assets/script/TileMap.cs
@@ -1,68 +1,55 @@
 ﻿using UnityEngine;
-using UnityEditor;
 using System.Collections;
 
-[RequireComponent (typeof(Atlas))]
 public class TileMap : MonoBehaviour {
-	/** a two-dimensional array of integers representing tilecodes at each map tile */
+    /** the tile atlas, a texture containing one or more tile images evenly spaced **/
+    public Texture2D atlas;
+
 	private int[,] map;
 	private GameObject[,] tileObjects;
 	private Sprite[,] sprites;
-	private Atlas atlas;
 
     // TODO: account for tile atlas padding
 
-	/** the width of the map in tiles */
-	public int mapWidth;
+    /** the width of all tiles in the atlas in pixels **/
+    public int tileWidth;
 
-	/** the height of the map in tiles */
-	public int mapHeight;
+    /** the height of all tiles in the atlas in pixels **/
+    public int tileHeight;
+
+	/** return the width of the tile atlas in tiles **/
+	public int AtlasWidth() {
+		return (atlas.width / tileWidth);
+	}
+	
+	/** return the height of the tile atlas in tiles **/
+	public int AtlasHeight() {
+		return (atlas.height / tileHeight);
+	}
 
 	public Rect getTileRect(int tilecode) 
 	{
-		return new Rect ((tilecode % atlas.RowLength) * atlas.tileWidth, 
-		                 ((atlas.ColLength - (tilecode / atlas.RowLength)) * atlas.tileHeight) - atlas.tileHeight, 
-		                 atlas.tileWidth, atlas.tileHeight);
+		return new Rect ((tilecode % AtlasWidth()) * tileWidth, ((AtlasHeight() - (tilecode / AtlasWidth())) * tileHeight) - tileHeight, tileWidth, tileHeight);
 	}
 
     public GameObject DrawTile(int x, int y, int tilecode)
     {
-		// alias atlas from Atlas component
-		if (atlas == null) {
-			atlas = this.GetComponent<Atlas> ();
-		}
-
 		// create an empty map if it does not already exist
 		if (map == null) {
-			map = new int[mapHeight, mapWidth];
-			tileObjects = new GameObject[mapHeight, mapWidth];
-			sprites = new Sprite[mapHeight, mapWidth];
+			map = new int[32, 32];
 		}
 
-		if (mapWidth > map.GetLength (1) || mapHeight > map.GetLength (0)) {
-			// create a new map that is larger than the original
-			int[,] newMap = new int[mapHeight, mapWidth];
-			GameObject[,] newTO = new GameObject[mapHeight, mapWidth];
-		    Sprite[,] newSprites = new Sprite[mapHeight, mapWidth];
+		if (tileObjects == null) {
+			tileObjects = new GameObject[32, 32];
+		}
 
-			// copy old map data to new map
-			for (int j = 0; j < map.GetLength(0); j++) {
-				for (int i = 0; i < map.GetLength(1); i++) {
-					newMap[j, i] = map[j, i];
-					newTO[j, i] = tileObjects[j, i];
-					newSprites[j, i] = sprites[j, i];
-				}
-			}
-
-			// replace old map with new map by reference
-			map = newMap;
-			tileObjects = newTO;
-			sprites = newSprites;
+		if (sprites == null) {
+			sprites = new Sprite[32, 32];
 		}
 
 		// check target against map dimensions
-		if (x > -1 && x < mapWidth &&
-			y > -1 && y < mapHeight) {
+		if (x > -1 && x < map.GetLength (1) &&
+			y > -1 && y < map.GetLength (0)) {
 			if (tilecode == -1) 
 			{
 				// disable this tile
@@ -72,7 +59,7 @@ public class TileMap : MonoBehaviour {
 				tileObjects[y, x].renderer.enabled = true;
 			}
 			
-			if (tilecode > atlas.LastTile) {
+			if (tilecode >= (AtlasWidth() * AtlasHeight())) {
 				throw new System.IndexOutOfRangeException("Tile code index is out of range of tile atlas.");
 			}
 			
@@ -82,7 +69,7 @@ public class TileMap : MonoBehaviour {
 				// destroy existing sprite data
 				DestroyImmediate(sprites[y, x]);
 				// assign new sprite data
-				sprites[y, x] = atlas.GetTile(tilecode);
+				sprites[y, x] = Sprite.Create(atlas, getTileRect(tilecode), Vector2.zero);
 				// update GameObject sprite with new sprite
 				((SpriteRenderer)(tileObjects[y, x].renderer)).sprite = sprites[y, x];
 				// return updated GameObject
@@ -97,8 +84,8 @@ public class TileMap : MonoBehaviour {
 				tileObjects[y, x].transform.parent = this.transform;
 
 				// create a new sprite
-				sprites[y, x] = atlas.GetTile(tilecode);
-				tileObjects[y, x].transform.Translate ((x / 100f) * atlas.tileWidth, (y / 100f) * atlas.tileHeight, 0f);
+				sprites[y, x] = Sprite.Create(atlas, getTileRect(tilecode), Vector2.zero);
+				tileObjects[y, x].transform.Translate ((x / 100f) * tileWidth, (y / 100f) * tileHeight, 0f);
 
 				// set new sprite to GameObject
 				((SpriteRenderer)(tileObjects[y, x].renderer)).sprite = sprites[y, x];
